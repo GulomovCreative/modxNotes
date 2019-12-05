8249e5b9d0c98a1adcd6af4dc80a26b9df7450ea
Bakhtovar Gulomov <gulomovcreative@gmail.com>
2019-12-05 17:17:43 +0700

    Add vehicle to add events

--- _build/build.php
+++ _build/build.php
@@ -285,6 +285,35 @@ class modxNotesPackage
 
 
     /**
+     * Add events
+     */
+    protected function events() {
+        $events = include($this->config['elements'] . 'events.php');
+        if (!is_array($events)) {
+            $this->modx->log(modX::LOG_LEVEL_ERROR, 'Could not package in events.');
+            return;
+        }
+        
+        $attributes = array(
+            xPDOTransport::PRESERVE_KEYS => true,
+            xPDOTransport::UPDATE_OBJECT => !empty($this->config['update']['events']),
+        );
+        foreach ($events as $k => $v) {
+            /** @var modEvent $event */
+            $event = $this->modx->newObject('modEvent');
+            $event->fromArray(array(
+                'name' => $v,
+                'service' => 6,
+                'groupname' => $this->config['name'],
+            ), '', true, true);
+            $vehicle = $this->builder->createVehicle($event, $attributes);
+            $this->builder->putVehicle($vehicle);
+        }
+        $this->modx->log(xPDO::LOG_LEVEL_INFO, 'Packaged in ' . count($events) . ' Plugins events.');
+    }
+
+
+    /**
      * Add plugins
      */
     protected function plugins()
