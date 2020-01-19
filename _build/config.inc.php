79d1739c1f4af9540a715fbf16ae0bd201d2b647
Bakhtovar Gulomov <gulomovcreative@gmail.com>
2020-01-19 13:35:59 +0700

    1.2.0-pl Release

--- _build/config.inc.php
+++ _build/config.inc.php
@@ -11,7 +11,7 @@ if (!defined('MODX_CORE_PATH')) {
 return [
     'name' => 'modxNotes',
     'name_lower' => 'modxnotes',
-    'version' => '1.1.0',
+    'version' => '1.2.0',
     'release' => 'pl',
     // Install package to site right after build
     'install' => true,
